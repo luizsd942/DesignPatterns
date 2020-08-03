4928a0536a27005943515fcd69d95d8903c1ffbc
Luiz Antonio <luiz.santos@trescon.com.br>
2020-08-03 12:34:23 -0300

    :tada: Add novo projeto para ChainsOfResponsability

---
+++ Patterns.ChainsResponsability/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Patterns.ChainsResponsability
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
